ca92647119744716fb9d2df5fdd5b0a9731d842c
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-07-04 19:33:05 -0700

    commiting 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,16 +2,9 @@
 #include <string.h>
 
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    *
- *
- *     * Return: Always 0
- *
- *      */
+ main - Entry point
+ Return: Always 0
+*/
 
 int main(void)
 

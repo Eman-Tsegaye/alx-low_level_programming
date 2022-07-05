bd621c3277689c4beac879bd3a93c2184e7a2079
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-07-04 18:48:53 -0700

    commiting 4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    *
+ *
+ *     * Return: Always 0
+ *
+ *      */
+
+int main(void)
+
+{
+char output[100];
+strcpy(output, "\"Programming is like building a multilingual puzzle");
+puts(output);
+return (0);
+}

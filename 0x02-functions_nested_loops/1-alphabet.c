df3f328eeee8d295121e33cd41c1be9a1d000f11
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-07-06 23:21:34 +0000

    Commiting 1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,16 @@
 #include "main.h"
 /**
- * main - calls a function that prints lower case
+ * print_alphabet - This function prints lower case
  *
  * Return: Always 0
  */
-void print_alphabet(void);
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
-}
+	int l;
 
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		_putchar(l);
+	}
+	_putchar('\n');
+}

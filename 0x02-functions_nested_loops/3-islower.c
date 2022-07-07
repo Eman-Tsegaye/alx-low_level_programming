0bcc47b5936a84d7585c3f7bdf3bc3e8030d42a7
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-07-07 00:12:08 +0000

    commiting 3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - checks if a character is a lower case
+ * @c: to be verified
+ * Return: return 0 or 1
+ */
+int _islower(int e)
+{
+	return (e >= 'a' && e <= 'z');
+}

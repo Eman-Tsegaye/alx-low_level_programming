8229c8a1f83ef9cf6d6d2dcb2b8cfa3c4a4b74c3
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-07-09 12:06:06 +0000

    Commiting 0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main -print if the number is positive or negative
+ * or zero
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int e = 0;
+
+	positive_or_negative(e);
+
+	return (0);
+}
+

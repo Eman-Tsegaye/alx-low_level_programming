e7c3d0e0bcc70155a9369d1125cc812ad137b0e3
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-07-11 12:05:30 +0000

    commiting 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _puts-prints a string
+ * @str:character
+ * @i:integer
+ * @len:integer
+ * Return:always 0
+ */
+void _puts(char *str)
+{
+	int i;
+	int len = 0;
+
+	while (*(str + len) != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < len; i++)
+		_putchar(*(str + i));
+	_putchar('\n');
+}
+
+
+

ec478247518fb8683aa879eb4e09313302261f35
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-07-12 01:38:10 +0000

    commiting 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rev_string-prints a string
+ * @s:character
+ * Return:always 0
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	int len = 0;
+	char new[99];
+
+	j = 0;
+
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+	for (i = len - 1; i >= 0 ; i--)
+	{
+		new[j] = *(s + i);
+		j++;
+	}
+	new[len] = '\0';
+	for (i = 0; i < len; i++)
+		s[i] = new[i];
+}

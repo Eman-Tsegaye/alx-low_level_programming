2c9a78a8465ff5470c1f3268b38af59610a538fc
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-07-12 04:17:46 +0000

    commiting 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * print_array-prints the second half of a string
+ * print_number-prints an integer
+ * @a:integer parameter
+ * @n:integer parameter
+ * Return:always 0
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+			printf("%c ", ',');
+	}
+}

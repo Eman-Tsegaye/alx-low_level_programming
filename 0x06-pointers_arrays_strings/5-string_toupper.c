30654aadd84fed545996cec371d39327baf18cc0
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-07-14 10:26:48 +0000

    commiting 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+ * string_toupper-converts a lower case character in a string to it's uppercase
+ * Return:integer
+ * @e:parameter
+ */
+
+char *string_toupper(char *e)
+{
+	int i;
+	int len = 0;
+
+	for (i = 0; *(e + i) != '\0'; i++)
+		len++;
+	for (i = 0; i < len; i++)
+	{
+		if (e[i] >= 97 && e[i] <= 122)
+			e[i] = ((e[i] + 32) + '0');
+	}
+return (e);
+}

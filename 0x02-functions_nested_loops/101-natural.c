87b5c69a38bd1e35cd9228ac2262ff8539304a00
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-07-15 04:58:21 +0000

    commiting 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,18 +6,13 @@
  */
 int main(void)
 {
-	int x[1024], y[1024], i, len = 0, sum = 0;
+	int x[1024], i, sum = 0;
 
 	for (i = 0; i < 1023; i++)
 	{
 		x[i] = i + 1;
 		if (!(x[i] % 3) || !(x[i] % 5))
-			y[i] = x[i];
-	}
-	for (i = 0; y[i]; i++)
-	{
-		len += 1;
-		sum = sum + y[i];
+			sum = sum + x[i];
 	}
 	printf("%d", sum);
 	printf("%s", "\n");

e4b22e2fcdf088a5fa014263fb2567dc1ecb1d79
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-07-15 06:43:57 +0000

    commiting 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,30 +1,28 @@
-#include "main.h"
 #include <stdio.h>
+
 /**
- * main-computes the sum of even numbers in the fibunacci series below 4000000
- * Return:Always 0
+ * main - prints the sum of the even-valued
+ * fabinocci number less than 400000
+ *
+ * Return: Nothing
  */
 int main(void)
 {
-	unsigned int x[35], y[35], i, sum = 0;
+	long a, b, sum;
 
-	for (i = 0; i < 35; i++)
-		y[i] = i + 1;
-	x[0] = 1;
-	x[1] = 2;
-	for (i = 0; i < 35; i++)
+	a = 1;
+	b = 2;
+	sum = 0;
+
+	while (a < 4000000)
 	{
-		x[i + 2] = x[i] + x[i + 1];
-		if ((x[i + 2] % 2) && (x[i + 2] <= 4000000))
-			sum = sum + x[i];
+		long c = a + b;
+
+		if (a % 2 == 0)
+			sum += a;
+		a = b;
+		b = c;
 	}
-	printf("%d", sum);
-	printf("%s", "\n");
-	i = 0;
-	while (i < 35)
-	{
-		printf("%d ,", x[i]);
-		i++;
-	}
-return (0);
+	printf("%ld\n", sum);
+	return (0);
 }

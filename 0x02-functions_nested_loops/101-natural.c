5c9adf08e3070f9be89576e9943fb2189994215e
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-07-15 04:45:52 +0000

    commiting 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main-computes the sum of all natural numbers below 1024
+ */
+void main(void)
+{
+	int x[1024], y[1024], i, len = 0, sum = 0;
+
+	for (i = 0; i < 1023; i++)
+	{
+		x[i] = i + 1;
+		if (!(x[i] % 3) || !(x[i] % 5))
+			y[i] = x[i];
+	}
+	for (i = 0; y[i]; i++)
+	{
+		len += 1;
+		sum = sum + y[i];
+	}
+	printf("%d", sum);
+	printf("%s", "\n");
+}

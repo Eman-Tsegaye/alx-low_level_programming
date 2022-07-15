262126f8d88c4e4a08b9edb8063891291589b09a
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-07-15 06:42:23 +0000

    commiting 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print fibonacci number
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int i;
+	long int fab[50];
+
+	fab[0] = 1;
+	fab[1] = 2;
+
+	printf("%ld, %ld, ", fab[0], fab[1]);
+
+	for (i = 2; i < 50; i++)
+	{
+		fab[i] = fab[i - 1] + fab[i - 2];
+		if (i == 49)
+		{
+			printf("%ld\n", fab[i]);
+		}
+		else
+		{
+			printf("%ld, ", fab[i]);
+		}
+	}
+	return (0);
+}

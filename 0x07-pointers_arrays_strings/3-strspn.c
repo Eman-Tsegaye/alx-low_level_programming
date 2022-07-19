0b90a8c3f171a1f942d1a4eec6ceb1218a1d443c
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-07-19 16:43:13 +0000

    commiting 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn-returns the number of bytes in the initial segment of a string
+ *@s:string
+ *@accept:list
+ *Return:integer
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, sum, len;
+
+	i = 0;
+	sum = 0;
+	len = 0;
+
+	while (*(accept + len) != '\0')
+		len += 1;
+	while ((s[i] >= 65 && s[i] <= 90) || (s[i] >= 97 && s[i] <= 122))
+	{
+		for (j = 0; j <= len; j++)
+		{
+			if (accept[i] == s[j])
+				sum += 1;
+		}
+		i += 1;
+	}
+	return (sum);
+}

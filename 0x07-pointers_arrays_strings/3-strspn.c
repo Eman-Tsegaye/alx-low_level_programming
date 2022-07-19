40ce5543bb36bed3d5dcb7a43d5dc1ee70459d00
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-07-19 17:21:40 +0000

    commiting 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,6 +12,7 @@ unsigned int _strspn(char *s, char *accept)
 	i = 0;
 	sum = 0;
 	len = 0;
+	j = 0;
 
 	/*while (*(accept + len) != '\0')*/
 	while ((s[len] >= 65 && s[len] <= 90) || (s[len] >= 97 && s[len] <= 122))
@@ -19,12 +20,16 @@ unsigned int _strspn(char *s, char *accept)
 
 	while ((s[i] >= 65 && s[i] <= 90) || (s[i] >= 97 && s[i] <= 122))
 	{
-		for (j = 0; j < len; j++)
+		while (j < len)
 		{
 			if (accept[i] == s[j])
+			{
 				sum += 1;
+			}
+			j += 1;
 		}
 		i += 1;
+		j = 0;
 	}
 	return (sum);
 }

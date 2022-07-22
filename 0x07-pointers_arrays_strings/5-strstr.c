5d5f34aae6ed7d85ed4371708d0103a4d7cc581f
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-07-22 16:18:53 +0000

    commiting tools and test files

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strstr-finds the first occurance of a given substring
+ * @haystack:string
+ * @needle:string
+ * Return:string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+}

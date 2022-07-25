1e5340abd2a9068063c1144fd5aa0a4db771a3f5
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-07-25 19:53:47 +0000

    commiting 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main-prints it's own name
+ * @argc:number of the argument pointers
+ * @argv:argument values
+ * Return:0 for successful
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+		printf("%s\n", "Error");
+	else
+		printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
+	return (0);
+}

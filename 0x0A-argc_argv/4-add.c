d535516d27457bfe551b71fdc4e98c1e1857b1d4
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-07-25 20:34:01 +0000

    commiting 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main-prints it's own name
+ * @argc:number of the argument pointers
+ * @argv:argument values
+ * Return:0 for successful
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, sum;
+
+	if (argc < 3)
+		printf("%s\n", "Error");
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				if (!(isdigit(argv[i][j])))
+				{
+					printf("Error\n");
+					return (1);
+				}
+				j++;
+			}
+		sum += atoi(argv[i]);
+		}
+	printf("%d\n", sum);
+	}
+	return (0);
+}

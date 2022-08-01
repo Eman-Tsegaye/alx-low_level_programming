b48cfa0d7a7ce833332b5976c6ffc21e36ae2107
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-08-01 20:00:27 +0000

    commiting dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_h
+#define MAIN_h
+
+/**
+ * struct dog-a struct with a dog attribute
+ * @name:string
+ * @age:float
+ * @owner:string
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

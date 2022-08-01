1773af86d0d8dd8deedc806105e0380d5f3151e2
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-08-01 21:21:24 +0000

    commiting 4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * new_dog-creates a dog
+ * @name:string
+ * @age:float
+ * @owner:string
+ * Return:pointer
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	return (d);
+}

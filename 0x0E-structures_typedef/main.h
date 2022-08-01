30ab1daba8f03ed758011d40ca035201b8eb07cc
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-08-01 21:56:47 +0000

    commiting .exe and testing files

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_h
+#define MAIN_h
+
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
+#endif

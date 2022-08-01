a0e799e86edea9480e7e785da5700ab15f1d8b75
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-08-01 20:20:34 +0000

    commiting 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 /**
  * init_dog-ititializes a variable of type dog
  * @d:pointer
@@ -12,4 +13,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
+	printf("%s","\n");
 }

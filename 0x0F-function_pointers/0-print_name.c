2795701a79ff7baab80126fe3319a07af1ab1d2d
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-08-05 11:55:52 +0000

    commiting 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name-points to the printing function
+ * @name:string
+ * @f:function pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}

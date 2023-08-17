d1d41143a34473eb453652833bec32d623d1b602
Eman-Tsegaye <emantsegaye3626@gmail.com>
2023-08-17 17:04:00 +0000

    commiting 6-sum_dlistint.c

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint -  function that returns the sum of all the data of a list
+ * @head: pointer to head list
+ *
+ * Return: sum, otherwise 0 if empty
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	if (head)
+		return (head->n + sum_dlistint(head->next));
+	return (0);
+}
